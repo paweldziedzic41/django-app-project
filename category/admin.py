70f80bc967767778c3d4a2a827e6887afbe6e72a
paweldziedzic41 <169774@stud.prz.edu,pl>
2023-06-25 21:13:50 +0200

    25.06.2023-21:13

--- category/admin.py
+++ category/admin.py
@@ -3,4 +3,11 @@ from . models import Category
 
 # Register your models here.
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    # Varaible sets field slug to copy from filed categoty_name
+    prepopulated_fields = {'slug':('category_name',)}
+    # Displaying category_name and slug on front page of categories
+    list_display = ('category_name', 'slug')
+
+
+admin.site.register(Category, CategoryAdmin)
--- greatkart/settings.py
+++ greatkart/settings.py
@@ -124,6 +124,10 @@ STATICFILES_DIRS = [
     'greatkart/static',
 ]
 
+# Media files configuration 
+MEDIA = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
