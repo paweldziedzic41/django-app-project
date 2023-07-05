e8dc3ccd8d87ed1d2daa8ae069d3ec78e0f49203
paweldziedzic41 <zxc24@o2.pl>
2023-07-05 11:19:35 +0200

    05.07.2023

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,33 @@
 from django import forms
 from .models import Account
+from django.http import HttpResponse
 
 class RegsitrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Enter Password',
+    }))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Confirm Password',
+    }))
     class Meta:
         model = Account
         fields = ['first_name', 'last_name', 'email', 'phone_number', 'password']
+        
+    # Function __init__ changes the CSS classes of all input fields    
+    def __init__(self,*args,**kwargs):
+        super(RegsitrationForm, self).__init__(*args,**kwargs)
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Enter First name'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Enter Last name'
+        self.fields['email'].widget.attrs['placeholder'] = 'Enter Email'
+        self.fields['phone_number'].widget.attrs['placeholder'] = 'Enter Phone number'
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
+
+    def clean(self):
+        cleaned_data = super(RegsitrationForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "Password does not match"
+            )
--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,35 @@
 from django.shortcuts import render
 from .forms import RegsitrationForm
+from .models import Account
 # Create your views here.
 
 def register(request):
-    form = RegsitrationForm()
+    if request.method == 'POST':
+        # Sending the user info into the registration form class
+        form = RegsitrationForm(request.POST)
+        # If the from has all the required fields
+        if form.is_valid():
+            # cleaned_data lets to fetch value from the request
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            phone_number = form.cleaned_data['phone_number']
+            password = form.cleaned_data['password']
+            
+            # Genrated username
+            username = email.split("@")[0]
+
+            # Creating a account
+            user = Account.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
+            user.phone_number = phone_number
+            user.save()
+    else:
+        # Only rendering the registration form
+        form = RegsitrationForm()
     context = {
         'form' : form,
     }
-    return render(request, 'accounts/register.html')
+    return render(request, 'accounts/register.html', context)
 
 def login(request):
     return render(request, 'accounts/login.html')
